e850723398eb2ccf898f2d59d1c8b85ca9d79d93
mlntcandy <31708294+mlntcandy@users.noreply.github.com>
2023-03-05 18:25:52 +0300

    [frontend] chore: Реорганизация файловой структуры (#2)

--- frontend/src/Screens.tsx
+++ frontend/src/Screens.tsx
@@ -1,23 +1,23 @@
 import { View, StyleSheet, useWindowDimensions, Text } from "react-native";
-import { ScreenType } from "../../App";
+import { ScreenType } from "../App";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from "react-native-reanimated";
-import { news, NewsScreen } from "../components/NewsScreen";
-import { Encyclopedia } from "../components/Encyclopedia";
-import { Maps } from "../components/Maps";
-import { Search } from "../components/Search";
+import { news, NewsScreen } from "./screens/NewsScreen";
+import { Encyclopedia } from "./screens/Encyclopedia";
+import { Maps } from "./screens/Maps";
+import { Search } from "./screens/Search";
 
 type Props = {
   currentScreen: ScreenType;
 };
 
 const screens: { [key in ScreenType]: JSX.Element } = {
-  Карта: <Maps/>,
+  Карта: <Maps />,
   Энциклопедия: <Encyclopedia />,
-  Поиск: <Search/>,
+  Поиск: <Search />,
   Новости: <NewsScreen data={news} />,
   Профиль: <Text>Я profile</Text>,
 };
@@ -26,11 +26,15 @@ export const Screens: React.FC<Props> = (props) => {
   const { width } = useWindowDimensions();
   const Screen: React.FC<React.PropsWithChildren> = ({ children }) => (
     <View style={[styles.screen, { width }]}>{children}</View>
-  ); 
+  );
 
   return (
     <View style={styles.wrapper}>
-        {<Screen key={props.currentScreen}>{screens[props.currentScreen as ScreenType]}</Screen>}
+      {
+        <Screen key={props.currentScreen}>
+          {screens[props.currentScreen as ScreenType]}
+        </Screen>
+      }
     </View>
   );
 };
---
+++ frontend/src/screens/Search.tsx
@@ -0,0 +1,41 @@
+import {
+  TextInput,
+  TouchableOpacity,
+  Button,
+  View,
+  StyleSheet,
+  ScrollView,
+  Image,
+  Text,
+} from "react-native";
+import { Mushroom, getAllMushrooms } from "../db/db";
+import MushList from "../db/mushrooms";
+
+export const Search: React.FC<Props> = (props) => {
+  let mushrooms = getAllMushrooms(MushList.list);
+
+  return (
+    <View style={styles.screen}>
+      <ScrollView>
+        <View>
+          <Text>just item</Text>
+        </View>
+      </ScrollView>
+      <TextInput style={styles.textInput} placeholder="Я хочу найти..." />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    height: "100%",
+  },
+  textInput: {
+    display: "flex",
+    backgroundColor: "#E3E3E3",
+    height: 45,
+    borderRadius: 30,
+    margin: 30,
+  },
+});
