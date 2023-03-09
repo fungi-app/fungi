ac2294fc2c09c4cfe5f36cd375ea03de3a27ab1d
mlntcandy <me@mlntcandy.com>
2023-03-10 02:40:51 +0300

    added prisma

--- backend/src/trpc/middleware.ts
+++ backend/src/trpc/middleware.ts
@@ -1,12 +1,17 @@
 import { inferAsyncReturnType, initTRPC } from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "@/trpc/router";
+import { prismaClient } from "@/db";
 
 // created for each request
 const createContext = ({
   req,
   res,
-}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+}: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
+  db: prismaClient,
+}); // no context
 
 export type Context = inferAsyncReturnType<typeof createContext>;
 
