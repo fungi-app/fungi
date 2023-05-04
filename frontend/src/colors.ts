57d2f274acc3e3814880f1a73dec3208eed8e20d
mlntcandy <me@mlntcandy.com>
2023-05-05 02:58:10 +0300

    working

--- frontend/src/colors.ts
+++ frontend/src/colors.ts
@@ -6,3 +6,40 @@ export const colors = {
   bg: "#FFFFFF",
   text: "#000000",
 };
+
+export type Theme = {
+  bg: string;
+  text: string;
+  secondaryBg: string;
+  fadedText: string;
+  secondary: string;
+  secondaryText: string;
+  primary: string;
+  separator: string;
+  metaBg: string;
+};
+
+export const schemes: { [key in "light" | "dark"]: Theme } = {
+  light: {
+    bg: "#fff",
+    text: "#000",
+    secondaryBg: "#FFE8C5",
+    fadedText: "rgba(0, 0, 0, 0.35);",
+    secondary: "#653D00",
+    secondaryText: "#B38550",
+    primary: "#DD6F1F",
+    separator: "rgba(0, 0, 0, 0.35)",
+    metaBg: "#FBDAB4",
+  },
+  dark: {
+    bg: "#000",
+    text: "#fff",
+    secondaryBg: "#452929",
+    fadedText: "rgba(255, 255, 255, 0.35)",
+    secondary: "#C79078",
+    secondaryText: "#9A796E",
+    primary: "#fff",
+    separator: "rgba(255, 255, 255, 0.2)",
+    metaBg: "#573737",
+  },
+};
---
+++ frontend/src/lib/theme.tsx
@@ -0,0 +1,18 @@
+import { ImageStyle, TextStyle, ViewStyle, useColorScheme } from "react-native";
+import { useStateStore } from "./store";
+import { Theme, schemes } from "../colors";
+type NamedStyles<T> = { [P in keyof T]: ViewStyle | TextStyle | ImageStyle };
+type StyleConstructor<T extends NamedStyles<T> | NamedStyles<any>> = (
+  colors: Theme
+) => T;
+
+export const useThemedStyle = <T extends NamedStyles<T> | NamedStyles<any>>(
+  construct: StyleConstructor<T>
+) => {
+  const selectedTheme = useStateStore((s) => s.selectedTheme);
+  const colorScheme = useColorScheme() ?? "light";
+
+  let scheme = selectedTheme === "system" ? colorScheme : selectedTheme;
+
+  return construct(schemes[scheme]);
+};
