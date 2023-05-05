8c5c3f87bcdaba1dfc0fc5f693a8e2fc52b70255
Vasyaslav <rkbrktyl@gmail.com>
2023-05-05 22:11:30 +0300

    Подключение бд с грибами к сайту

---
+++ frontend/src/screens/Profile.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+import { BottomMenuPad } from "../components/BottomMenu";
+import { TopBarPad } from "../components/TopBar";
+import { trpc } from "../lib/trpc";
+import { tunnel } from "../lib/tunnel";
+
+export const Profile: React.FC = () => {
+  const hello = trpc.example.hello.useQuery({ name: "Ivan" });
+
+  return (
+    <View>
+      <TopBarPad />
+      <Text>
+        profile {hello.data ?? "Loading"}, tunnel {tunnel}
+      </Text>
+      <BottomMenuPad />
+    </View>
+  );
+};
